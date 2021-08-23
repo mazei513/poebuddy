104c06ee236861c82f4e0d405e5e4be7fa7fc5d2
Azraei Yusof <mazei513@gmail.com>
2021-08-23 15:29:00 +0800

    Added querying from PoE API for stash data

--- main.go
+++ main.go
@@ -1,7 +1,80 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello world")
+	baseUrlFlag := flag.String("url", "https://www.pathofexile.com", "base API URL")
+	flag.Parse()
+
+	acc := flag.Arg(0)
+	if acc == "" {
+		fmt.Println("account name is required")
+	}
+
+	poeURL := *baseUrlFlag
+	u, err := url.Parse(poeURL)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	u.Path = "character-window/get-stash-items"
+
+	v := u.Query()
+	v.Set("accountName", acc)
+	v.Set("realm", "pc")
+	v.Set("league", "Expedition")
+
+	u.RawQuery = v.Encode()
+
+	resp, err := http.Get(u.String())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	stashResp := stashResp{}
+	err = json.NewDecoder(resp.Body).Decode(&stashResp)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%d tabs\n", len(stashResp.Tabs))
+	for _, t := range stashResp.Tabs {
+		if t.Selected {
+			fmt.Printf("Current tab: %s\n", t.N)
+			fmt.Println("Items:")
+			for _, i := range stashResp.Items {
+				f := "  %s\n"
+				a := []interface{}{i.TypeLine}
+				if i.Name != "" {
+					f = "  %s, %s\n"
+					a = []interface{}{i.Name, i.TypeLine}
+				}
+				fmt.Printf(f, a...)
+			}
+		}
+	}
+	fmt.Printf("%d items\n", len(stashResp.Tabs))
+}
+
+type stashResp struct {
+	Tabs []struct {
+		N        string `json:"n"`
+		Type     string `json:"type"`
+		Selected bool   `json:"selected"`
+	} `json:"tabs"`
+	Items []struct {
+		Name     string `json:"name"`
+		TypeLine string `json:"typeLine"`
+		BaseType string `json:"baseType"`
+	} `json:"items"`
 }
